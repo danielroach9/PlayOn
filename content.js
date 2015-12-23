3cda674d0e553a95f8770010d79a853aca13eaa7
Daniel Roach <dannycr09@hotmail.com>
2015-12-23 13:39:41 -0500

    Starting point

--- content.js
+++ content.js
@@ -1,23 +1,37 @@
-function kissanime() {}
+	// var vid = document.getElementById("my_video_1_html5_api");
+
+	// if(vid === null || vid === undefined) {
+	// 	alert("NO VIDEO FOUND");
+	// }
+	// else {
+	// 	//vid.addEventListener("onended", kissanime);
+	// 	vid.addEventListener("onended", function() {
+	// 		var nextArrow = document.getElementById("btnNext");
+	// 		var nextEpLink = nextArrow.parentElement.src;
+
+	// 		window.location.href = 'nextEpLink';
+	// 		});
+	// }
+function kissanime() {
+
+	// use the onended property to see when the video has actually finished
+	// we probably need an eventlistener to work around that
+
+	// var nextArrow = document.getElementById("btnNext");
+	// var nextEpLink = nextArrow.parentElement.src;
+
+	// window.location.href = 'nextEpLink';
+}
+
+function placeKissAnimeFunction() {
+	// var vid = document.getElementById("my_video_1_html5_api");
+
+	// if(vid === null || vid === undefined) {
+	// 	alert("NO VIDEO FOUND");
+	// 	return;
+	// }
+	// else
+	// 	vid.setAttribute("onended", "kissanime()");
+}
 
 function toonova() {}
-
-// function changeColor() {
-// 	var btn = document.getElementById("button");
-// 	btn.color = "black";
-// }
-
-function enableButton() {
-	var btn = document.getElementById("button");
-
-	if(btn.innerHTML == 'Disable') {
-		btn.innerHTML = 'Enable';
-		btn.setAttribute("background-color", "green");
-	}
-
-	else {
-		btn.innerHTML = 'Disable';
-		btn.setAttribute("background-color", "red");
-
-	}
-}
